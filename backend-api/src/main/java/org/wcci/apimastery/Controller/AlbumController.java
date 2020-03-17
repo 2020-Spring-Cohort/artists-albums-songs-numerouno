6d47276ec39d5eca8765f8a4260c390a2a362c36
fpemba <fatuma.pemba@yahoo.com>
2020-03-17 13:51:45 -0400

    added single song end point
    created single song endpoint and song storage class

--- backend-api/src/main/java/org/wcci/apimastery/Controller/AlbumController.java
+++ backend-api/src/main/java/org/wcci/apimastery/Controller/AlbumController.java
@@ -1,23 +1,31 @@
 package org.wcci.apimastery.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.wcci.apimastery.Album;
 import org.wcci.apimastery.AlbumRepository;
+import org.wcci.apimastery.Artist;
+import org.wcci.apimastery.SongStorage;
 
 import java.util.Collection;
 
 @RestController
+@RequestMapping("albums")
 public class AlbumController {
     private AlbumRepository albumRepository;
+
+
 
     public AlbumController (AlbumRepository albumRepository) {
 
         this.albumRepository = albumRepository;
     }
-    @RequestMapping("/albums")
+    @GetMapping("")
     public Collection<Album> retrieveAlbums() {
          return (Collection<Album>) albumRepository.findAll();
     }
+
 
 }
