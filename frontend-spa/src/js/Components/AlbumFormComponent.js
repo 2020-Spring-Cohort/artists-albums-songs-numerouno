deb5a68e26b695e9aabb62695fc96bc7884179f5
[Nadir Carpenter] <[nayhova@gmail.com]>
2020-03-25 19:08:23 -0400

    frontEnd work

--- frontend-spa/src/js/Components/AlbumFormComponent.js
+++ frontend-spa/src/js/Components/AlbumFormComponent.js
@@ -1,5 +1,56 @@
-const renderNewAlbumForm = () =>{
+import {
+    renderAlbum
+} from './AlbumListComponent.js';
+
+
+
+const renderNewAlbumForm = () => {
 
     const title = document.createElement('p');
     title.innerText = 'Add a new Album';
+
+    const albumTitle = document.createElement('input');
+    albumTitle.setAttribute('placeholder', 'Album Title');
+    const albumRecordLabel = document.createElement('input');
+    albumRecordLabel.setAttribute('placeholder', 'Record Label');
+    const albumImage = document.createElement('input');
+    albumImage.setAttribute('placeholder', 'Album Image');
+    const albumArtist = document.createElement('input');
+    albumArtist.setAttribute('placeholder', 'Album Artist');
+
+    const submitBtn = document.createElement('button');
+    submitBtn.innerText = 'Save';
+
+    title.appendChild(albumTitle);
+    title.appendChild(albumRecordLabel);
+    title.appendChild(albumImage);
+    title.appendChild(albumArtist);
+    title.appendChild(submitBtn);
+
+    submitBtn.addEventListener('click', () => {
+        collectData();
+    })
+
+    const collectData = () => {
+
+        const artist = {
+            "Title": albumTitle.value,
+            "recordLabel": albumRecordLabel.value,
+            "image": albumImage.value,
+            "artist": albumArtist.value
+        }
+        fetch('http://localhost:8080/albums', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(album)
+        }).then(() => renderAlbum());
+    }
+
+    return title;
+}
+
+export{
+    renderNewAlbumForm
 }
